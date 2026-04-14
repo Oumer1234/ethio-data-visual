b7154641c5f4ff6ef6091f4c1574c39bd6386208
Oumer1234 <147939828+Oumer1234@users.noreply.github.com>
2026-04-14 01:49:39 -0400

    Add Globe component and update Home page layout

---
+++ components/Globe/Globe.tsx
@@ -0,0 +1,19 @@
+function Globe() {
+  return (
+    <div
+      style={{
+        width: "80%",
+        height: "100%",
+        backgroundColor: "skyblue",
+        border: "2px solid navy",
+
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      I am a box
+    </div>
+  );
+}
+export default Globe;
